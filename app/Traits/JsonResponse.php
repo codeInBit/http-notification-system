15e9d3e7d885f3f8bbd6db5beb26a37d43e152d8
Olanrewaju Abidogun <olanrewaju.abidogun@gmail.com>
2021-04-05 11:24:24 +0100

    Chore: Generate and modify laravel stub and also trait file to handle UUID usage as primary keys and uniform API json responses

---
+++ app/Traits/JsonResponse.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Traits;
+
+use App\Helpers\DatatableForResource;
+use Exception;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+trait JsonResponse
+{
+    public function successResponse($data, $statusCode = Response::HTTP_OK): \Illuminate\Http\JsonResponse
+    {
+        $response = [
+            "data" => $data,
+        ];
+        return response()->json(
+            ["data" => $data],
+            $statusCode
+        );
+    }
+
+    public function errorResponse($data = null, $message = null, $statusCode = Response::HTTP_BAD_REQUEST): \Illuminate\Http\JsonResponse
+    {
+        return response()->json([
+            "data" => $data,
+            "message" => $message
+        ], $statusCode);
+    }
+
+    public function fatalErrorResponse(Exception $e, $statusCode = Response::HTTP_INTERNAL_SERVER_ERROR): \Illuminate\Http\JsonResponse
+    {
+        $line = $e->getTrace();
+
+        $error = [
+            "message" => $e->getMessage(),
+            "trace" => $line[0],
+            "mini_trace" => $line[1]
+        ];
+
+        if (strtoupper(config("APP_ENV")) === "production") $error = null;
+
+
+        return response()->json([
+            "message" => "Oops! Something went wrong on the server",
+            "error" => $error
+        ], $statusCode);
+    }
+}
